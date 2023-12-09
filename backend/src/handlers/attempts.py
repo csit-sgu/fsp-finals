11330e9c0153cfb3dcd776ac773d92203bacde88
ArtemiyGranat <artemiigranat@gmail.com>
2023-12-09 12:54:31 +0300

    feat: асинхронный асинк

--- backend/src/handlers/attempts.py
+++ backend/src/handlers/attempts.py
@@ -1,4 +1,5 @@
 import json
+import asyncio
 from typing import Annotated, List
 from deps import get_current_user
 
@@ -23,6 +24,86 @@ async def get_attempts():
     return await ctx.attempt_repo.get_many()
 
 
+async def make_attempt_helper(answer, overall_feedback, total_score):
+    original_block: entities.Block | None = await ctx.block_repo.get_one(
+            "block_id", answer.block_id
+        )
+    if original_block is None:
+        log.warn(f"Block {answer.block_id} not found")
+        return
+
+    options = json.loads(original_block.payload)["options"]
+    if (
+        original_block.block_type == BlockType.MULTIPLE_CHOICE
+        and not isinstance(answer.answer, list)
+    ):
+        raise HTTPException(
+            status_code=400,
+            detail=f"Expected list of answers in {original_block.block_id}",
+        )
+    if (
+        original_block.block_type != BlockType.MULTIPLE_CHOICE
+        and isinstance(answer.answer, list)
+    ):
+        raise HTTPException(
+            status_code=400,
+            detail=f"Expected single answer in {original_block.block_id}",
+        )
+
+    if not isinstance(answer.answer, list):
+        answer.answer = [answer.answer]
+
+    prompt_block_str = f"Вопрос: {original_block.problem}"
+    if original_block.block_type == BlockType.FREE_ANSWER:
+        prompt_block_str += f'\nЯ ответил {answer.answer}. Объясни, правильный ли это ответ и почему.'
+    elif original_block.block_type == BlockType.MULTIPLE_CHOICE:
+        prompt_block_str += (
+            f'\nВарианты ответа: {", ".join(options.keys())}'
+        )
+        prompt_block_str += f'\nЯ ответил {", ".join(answer.answer)}. Объясни, правильный ли это ответ и почему.'
+    else:
+        prompt_block_str += (
+            f'\nВарианты ответа: {", ".join(options.keys())}'
+        )
+        prompt_block_str += f'\nЯ ответил {answer.answer}. Объясни, правильный ли это ответ и почему.'
+
+    block_score = 0
+    for ans in answer.answer:
+        try:
+            chosen_option = options[ans.strip()]
+        except KeyError:
+            continue
+        block_score += chosen_option["score"]
+
+    total_score += block_score
+
+    completion = ""
+    if block_score != 1:
+        completion = (
+            (
+                await ctx.openai_client.chat.completions.create(
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": prompt_block_str,
+                        }
+                    ],
+                    model="gpt-4",
+                )
+            )
+            .choices[0]
+            .message.content
+        )
+
+    attempt_feedback = AttemptFeedback(
+        feedback=completion,
+        correct=(block_score == 1),
+        score=block_score,
+    )
+
+    overall_feedback.append(attempt_feedback)
+
+
 @attempt_router.post("/attempt")
 async def make_attempt(
     user: Annotated[User, Depends(get_current_user)], attempt: AttemptFrontend
@@ -40,83 +121,10 @@ async def make_attempt(
     total_score = 0
     overall_feedback: List[AttemptFeedback] = list()
 
+    batch = list()
     for answer in attempt.answers:
-        original_block: entities.Block | None = await ctx.block_repo.get_one(
-            "block_id", answer.block_id
-        )
-        if original_block is None:
-            log.warn(f"Block {answer.block_id} not found")
-            continue
+        batch.append(make_attempt_helper(answer, overall_feedback, total_score))
 
-        options = json.loads(original_block.payload)["options"]
-        if (
-            original_block.block_type == BlockType.MULTIPLE_CHOICE
-            and not isinstance(answer.answer, list)
-        ):
-            raise HTTPException(
-                status_code=400,
-                detail=f"Expected list of answers in {original_block.block_id}",
-            )
-        if (
-            original_block.block_type != BlockType.MULTIPLE_CHOICE
-            and isinstance(answer.answer, list)
-        ):
-            raise HTTPException(
-                status_code=400,
-                detail=f"Expected single answer in {original_block.block_id}",
-            )
-
-        if not isinstance(answer.answer, list):
-            answer.answer = [answer.answer]
-
-        prompt_block_str = f"Вопрос: {original_block.problem}"
-        if original_block.block_type == BlockType.FREE_ANSWER:
-            prompt_block_str += f'\nЯ ответил "{answer.answer}". Объясни, правильный ли это ответ и почему.'
-        elif original_block.block_type == BlockType.MULTIPLE_CHOICE:
-            prompt_block_str += (
-                f'\nВарианты ответа: {", ".join(options.keys())}'
-            )
-            prompt_block_str += f'\nЯ ответил "{", ".join(answer.answer)}". Объясни, правильный ли это ответ и почему.'
-        else:
-            prompt_block_str += (
-                f'\nВарианты ответа: {", ".join(options.keys())}'
-            )
-            prompt_block_str += f'\nЯ ответил "{answer.answer}". Объясни, правильный ли это ответ и почему.'
-
-        log.debug(prompt_block_str)
-        block_score = 0
-        for ans in answer.answer:
-            chosen_option = options[ans.strip()]
-            if chosen_option is None:
-                continue
-            block_score += chosen_option["score"]
-
-        total_score += block_score
-
-        completion = ""
-        if block_score != 1:
-            completion = (
-                (
-                    await ctx.openai_client.chat.completions.create(
-                        messages=[
-                            {
-                                "role": "user",
-                                "content": prompt_block_str,
-                            }
-                        ],
-                        model="gpt-4",
-                    )
-                )
-                .choices[0]
-                .message.content
-            )
-
-        attempt_feedback = AttemptFeedback(
-            feedback=completion,
-            correct=(block_score == 1),
-            score=block_score,
-        )
-
-        overall_feedback.append(attempt_feedback)
+    batch = await asyncio.gather(*batch)
 
     return overall_feedback
